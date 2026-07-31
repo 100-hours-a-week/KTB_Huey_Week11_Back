6d5cca7c3a2eec0b87dd6a617c4cf1143cd80946
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2026-07-31 16:05:56 +0900

    separated frontend

--- frontend/auth.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    if (localStorage.getItem("user_id") === null) {
-        console.log("unauthorized");
-        window.location = "login.html";
-    }
-});
